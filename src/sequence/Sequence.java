5833c0bb96a47639bdd78a79adfc72aa5f151c96
cscase <cscottcase@gmail.com>
2015-08-22 22:27:34 -0500

    Added setter for header property

--- src/sequence/Sequence.java
+++ src/sequence/Sequence.java
@@ -4,25 +4,6 @@ import java.util.Scanner;
 
 /**
  * @author scottcase
- * COSC 2033 Section AA, Summer 2015
- * Week 15
- * Personal programming assignment
- * Author Scott Case
- * Modified 08 / 19 / 2015
- *
-Quick summary:
-This program opens .fasta files (see wikipedia entry for info about this simple
-file format) and parses their contents into an object class called Sequence,
-or one of two subclasses for RNA or DNA sequences, as appropriate. Each
-sequence is displayed on its own tab, and information about the selected
-sequence is displayed about the tab pane. I'm planning to add more
-functionality in the future that will make it more useful, so this is just a
-starting point.
-
-I'm submitting the .jar itself, which contains both the .class files and the
-source code for the project. The source code is in the src folder in the .jar.
-I'm also submitting some .fasta files that can be opened for testing the
-project and seeing how it works. Finally, I'm also including UML diagrams.
  */
 
 // The Sequence class is a generic object that can hold any kind of sequence.
@@ -30,7 +11,7 @@ project and seeing how it works. Finally, I'm also including UML diagrams.
 
 public class Sequence {
     protected final String seq;
-    private final String header;
+    private String header;
 
     //Constructor if no header specified. Class doesn't have a no-arg constr.
     public Sequence(String seq) {
@@ -50,6 +31,10 @@ public class Sequence {
 
     public String getHeader() {
         return header;
+    }
+
+    public void setHeader(String newHeader) {
+        this.header = newHeader;
     }
 
     // return the contents of the entire DNA sequence
